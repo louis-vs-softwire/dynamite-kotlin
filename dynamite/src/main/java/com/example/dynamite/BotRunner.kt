0e6b1676b4a69ecc62a82e94a0b42374acb2505f
Louis Van Steene <Louis.VanSteene@softwire.com>
2021-07-09 14:14:52 +0100

    Improve bot runner

--- dynamite/src/main/java/com/example/dynamite/BotRunner.kt
+++ dynamite/src/main/java/com/example/dynamite/BotRunner.kt
@@ -3,17 +3,25 @@ package com.example.dynamite
 import com.softwire.dynamite.runner.*
 
 object BotRunner {
-//    @JvmStatic
-//    fun main(args: Array<String>) {
-//        val results = (1..10).map { DynamiteRunner.playGames { MyBot() } }
-//        val avgWins = results.map { x -> x.results.sumBy { (it.result == "WIN").compareTo(false) } }.average()
-//        val avgLosses = results.map { x -> x.results.sumBy { (it.result == "LOSE").compareTo(false) } }.average()
-//        print("\nAverage Wins: $avgWins\nAverage Losses: $avgLosses\n")
-//    }
-
-    // for uploading to website
     @JvmStatic
     fun main(args: Array<String>) {
-        val results: Results = DynamiteRunner.playGames { MyBot() }
+        //run()
+        test()
+    }
+
+    private fun run() {
+        DynamiteRunner.playGames { MyBot() }
+    }
+
+    private fun test() {
+        val results = (1..10).map { DynamiteRunner.playGames { MyBot() } }
+        val avgWins = getAvg(results, "WIN")
+        val avgLosses = getAvg(results, "LOSE")
+        val avgDraws = getAvg(results, "DRAW")
+        print("\nAverage Wins: $avgWins\nAverage Losses: $avgLosses\nAverage Draws: $avgDraws\n")
+    }
+
+    private fun getAvg(results: List<Results>, outcome: String) : Double {
+        return results.map { x -> x.results.sumBy { (it.result == outcome).compareTo(false) } }.average()
     }
 }
